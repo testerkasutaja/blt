636a90e0ca0a1c5aff04107d8f23d34487db5443
Anneliis <anneliishalling@gmail.com>
2015-10-28 22:10:17 +0200

    Start
    First commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<head>
+	<meta charset="utf-8">
+	<title>Käänded</title>
+	<meta name="description" content=" kaanded">
+  <meta name="author" content="Anneliis Halling">
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="//code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet">
+	
+	<script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+	<script src="js/scripts.js"></script>
+	
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="guide">
+			<div class="well">
+			<h2 class="text-divider"><span>Õpime käändeid</span></h2>
+				<p>
+					Mängu alustades näete lauset, kus on üks sõna puudu. Sõna on antud nimetavas käändes. Pane sõna õigesse käändesse ning vajuta paremale noolt. Vihjeks on antud ka kääne!
+				</p>
+			</div>
+		</div>
+		
+		<div class="startdown">
+			<button onclick="location.href='game.php';" type="button" class="btn btn-primary ribbon startbtn" action="/game.php">Alusta</button>
+		</div>
+	</div>
+	
+
+</body>
