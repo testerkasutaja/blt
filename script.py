c4f450ea2877f2830eb29560ec658c47f22e6343
testerkasutaja <testerkasutaja@gmail.com>
2016-01-20 13:09:18 +0200

    valmis testimata
    osade korpusfailidega ei tööta

--- script.py
+++ script.py
@@ -29,39 +29,12 @@ def xml_formatting(elem, level=0):
     if level and (not elem.tail or not elem.tail.strip()):
       elem.tail = i  
   
-def structureCompatibilityLevel1(list):
-  #proovi struktuur - tegelik strukutuuri loovad funktsiooid getBestPOSCombination ja combintolist
-  structure = {'P':{'S':{'V':{'S':True}},'D':{'A':{'S':True}}},'S':{'S':{'D':{'S':True}}},'H':{'V':{'S':{'S':True}}}}
-  if 'V' in list:
-    if len(list)==3 :
-      a,b,c = list[0],list[1],list[2]
-      try:
-        structure[a][b][c]
-        return True
-      except KeyError:
-        return False
-    if len(list)==4 :
-      a,b,c,d = list[0],list[1],list[2],list[3]
-      try:
-        structure[a][b][c][d]
-        return True 
-      except KeyError:
-        return False
-
-def structureCompatibilityLevel2(list):
-  if 'V' in list and 'S' in list:
-    return True
-
-#P = ['P','S','V','S']
-
-#print(structureCompatibilityLevel1(P))
 
 def getPartOfSpeech(list):                                   #kontroll, et kõik sõnad oleks üheselt määratud
   partofspeech = []
   for word in list:
     morf_l = analyze(word)
     morf_l2 = morf_l[0]['analysis']
-    
     if len(morf_l2) != 1:                           #üheselt määratavuse kontroll
       return []
     else:
@@ -77,8 +50,11 @@ def listtostring(list):
 #print(listtostring(['a','b','c']))
 
 #KÕIKIDEST FAILIDEST POPULAASREMATE KOMBINATSIOONIDE LEIDMINE esimese leveli jaoks (neljasõnalised laused)
-def getBestPOSCombination(case_dict, files):          
-  combinations = {}
+def getBestPOSCombination(files):          
+  combinations3words = {}
+  combinations4words = {}
+  structure_with_sentences_level1 = {}
+  sentences_level2 = []
   for pathStr in files:
     print(pathStr)
     tree = ET.parse(pathStr)
@@ -90,134 +66,119 @@ def getBestPOSCombination(case_dict, files):
         sen_list = morf_sen.split(' ')
         sen_len = len(sen_list)
         partofspeech = getPartOfSpeech(sen_list)
-        if partofspeech != [] and sen_len > 3 and sen_len <5 and 'Z' not in partofspeech and 'V' in partofspeech and ('S' in partofspeech or 'A' in partofspeech or 'U' in partofspeech  or 'C' in partofspeech or 'N' in partofspeech or 'O' in partofspeech or 'p' in partofspeech):
+        if len(partofspeech)>0 and sen_len > 2 and sen_len < 5 and 'Z' not in partofspeech and 'V' in partofspeech:
           partofspeech_str = listtostring(partofspeech)      
-          if partofspeech_str in combinations:
-            old = combinations[partofspeech_str]
-            new = old +1
-            combinations[partofspeech_str] = new
-            #print(partofspeech_str)
+          if partofspeech_str in combinations3words or partofspeech_str in combinations4words :
+            if sen_len == 3:
+              old = combinations3words[partofspeech_str]
+              new = old +1
+              combinations3words[partofspeech_str] = new
+            else:
+              old = combinations4words[partofspeech_str]
+              new = old +1
+              combinations4words[partofspeech_str] = new
+            structure_with_sentences_level1[partofspeech_str].append(sen)
           else:
-            combinations[partofspeech_str] = 1
-            #print(partofspeech_str)
-  sorted_com = sorted(combinations.values())
-  maximum = sorted_com[-1]
-  print(maximum)
-  return combinations
+            if sen_len==3:
+              combinations3words[partofspeech_str] = 1
+            else:
+              combinations4words[partofspeech_str] = 1
+            structure_with_sentences_level1[partofspeech_str] = []
+            structure_with_sentences_level1[partofspeech_str].append(sen)
 
-comb = {'S, V, P, A': 3, 'S, V, S, V': 2}
+        elif partofspeech != [] and sen_len > 4 and sen_len > 10 and 'V' in partofspeech and 'S' in partofspeech:
+           sentences_level2.append(sen)
+  #print(structure_with_sentences_4word)
+  #print(sentences_morethan4word)
+  return (combinations3words,combinations4words,structure_with_sentences_level1,sentences_level2)
 
-def combintotree(combinations):
-  structure = {}
-  sorted_com = sorted(combinations.values())
-  maximum = sorted_com[-1]
-  minimum = 2
-  for k, v in combinations.items():
-    p={}
-    p2={}
-    if v <= maximum and v >= minimum:
-      key_list = k.split(', ')
 
-      
-  '''for i in  range(len(key_list)-1,-1,-1):
-        #print(i)
-        if i == len(key_list)-1:
-          p[key_list[i]] = True   
-        else:
-          p2[key_list[i]] = p
-          print('p2')
-          print(p2)
-          p=p2
-          p2 = {}
-      print(p)'''    
+def getFinalSentenceListLevel1(combinations,structure_with_sentences_level1):
+  level1= []
+  if len(combinations)>0:
+    sorted_com = sorted(combinations.values())
+    maximum = sorted_com[-1]
+    minimum = (maximum//1.5)
+    print(minimum)
+    print(combinations)
+    for k, v in combinations.items():
+      if v >= minimum:
+        listofsentences = structure_with_sentences_level1.get(k)
+        for level1_sentence in listofsentences:
+          level1.append(level1_sentence)
+  return(level1)
+    
+  
 
   
-#combintotree(comb)
-  
-def runCaseAnalys(case_dict, pathStr, file_name):
+def runCaseAnalys(case_dict, list_of_sentences, file_name):
     go = False
-    tree = ET.parse(pathStr)
-    root = tree.getroot()
     countid=0
     content = ET.Element('content')
     tree2 = ElementTree(content)
-    for elem in root.findall('.//{http://www.tei-c.org/ns/1.0}s'):
-        sen = elem.text
+    if len(list_of_sentences)>0:
+      for sen in list_of_sentences:
         sen = re.sub('^ | $', '', sen)
         morf_sen = re.sub('(( (,|\.|!|\?|%|#|"))|" )', '', sen)
         sen_list = morf_sen.split(' ')
         sen_len = len(sen_list)
         partofspeech = getPartOfSpeech(sen_list)              # lause struktuur
-        #compatibility1 = structureCompatibilityLevel1(partofspeech)
-        #compatibility2 = structureCompatibilityLevel2(partofspeech)
-        if sen_len > 2 and sen_len <10 and len(partofspeech) > 0: # and (compatibility1 == True or compatibility2 == True):                           #Lause on 3-4 sõna ja on unikaalne(kui partofspeech on 
-          #print(morf_sen)
-          #print(partofspeech)
-          for word in sen_list:
-            morf_l = analyze(word)
-            morf_l2 = morf_l[0]['analysis']
-            if len(morf_l2) == 1: #pole vaa kuna kontroll toimub jube eelnevalt ära        #üheselt määratud sobib otsitavaks käändsõnaks
-              b = morf_l2[0]
-              case_info =(b['form']).split(' ')
-              if len(b['root_tokens']) == 1:
-                nominative = b['root_tokens'][0]
-              else:
-                nominative = b['root_tokens'][0]+b['root_tokens'][1]
-              if case_info[0]=='adt':                           #Lühikesisseütlev
-                casename = case_info[0]
-                sg_pl='sg'
-                go = True
-              elif len(case_info)== 2 and case_info[0] in case_dict and case_info[1] in case_dict: #kui on käändsõna
-                sg_pl = case_info[0]                            # ainus v mitmus   
-                casename = case_info[1]                         # kääne
-                go = True
-              if go == True:
-                sen_x = re.sub(word,'%%%',sen)
-                info = SubElement(content,'info')               #XML loomine
-                #if sen_len <5:# and  compatibility1 == True:
-                #  info.set('level','1')
-                #elif compatibility2 == True:
-                #  info.set('level','2')
-                info.set('id', str(countid))
-                countid = countid + 1
-                s = SubElement(info,'s')
-                nr = SubElement(info,'nr')
-                case = SubElement(info,'case')
-                n = SubElement(info,'n')
-                synt = synthesize(nominative, form = sg_pl+' '+casename, phonetic=False)      #kontorll kas leidub rohkem kui üks vastus
-                if len(synt)>1:
-                  for nom in synt:
-                    nom = re.sub('_','',nom)
-                    answer = SubElement(info, 'answer')
-                    answer.text= nom 
-                else:
-                  answer = SubElement(info, 'answer')
-                  answer.text = word
-                n.text = nominative
-                nr.text = case_dict[sg_pl]  
-                case.text = case_dict[casename]
-                s.text = sen_x
-                go = False
+        for word in sen_list:
+          morf_l = analyze(word)
+          morf_l2 = morf_l[0]['analysis']
+          b = morf_l2[0]
+          case_info =(b['form']).split(' ')
+          if len(b['root_tokens']) == 1:
+            nominative = b['root_tokens'][0]
+          else:
+            nominative = b['root_tokens'][0]+b['root_tokens'][1]
+          if case_info[0]=='adt':                           #Lühikesisseütlev
+            casename = case_info[0]
+            sg_pl='sg'
+            go = True
+          elif len(case_info)== 2 and case_info[0] in case_dict and case_info[1] in case_dict: #kui on käändsõna
+            sg_pl = case_info[0]                            # ainus v mitmus   
+            casename = case_info[1]                         # kääne
+            go = True
+          if go == True:
+            sen_x = re.sub(word,'%%%',sen)
+            info = SubElement(content,'info')               #XML loomine
+            info.set('id', str(countid))
+            countid = countid + 1
+            s = SubElement(info,'s')
+            nr = SubElement(info,'nr')
+            case = SubElement(info,'case')
+            n = SubElement(info,'n')
+            synt = synthesize(nominative, form = sg_pl+' '+casename, phonetic=False)      #kontorll kas leidub rohkem kui üks vastus
+            if len(synt)>1:
+              for nom in synt:
+                nom = re.sub('_','',nom)
+                answer = SubElement(info, 'answer')
+                answer.text= nom 
+            else:
+              answer = SubElement(info, 'answer')
+              answer.text = word
+            n.text = nominative
+            nr.text = case_dict[sg_pl]  
+            case.text = case_dict[casename]
+            s.text = sen_x
+            go = False
                       
     xml_formatting(content)
     tree2.write(file_name,'utf8')
 
 
-runCaseAnalys(case_dict, 'proov.xml', 'laused.xml' )
 
 
 
 
-#print(getBestPOSCombination(case_dict, 'proov.xml'))
-#print(getBestPOSCombination(case_dict,files))
-#runCaseAnalys(case_dict, 'Eesti_ilukirjandus/ilukirjandus/Eesti_ilukirjandus_1990/ilu_viljakyla.tasak.xml', 'laused.xml' )
-#runCaseAnalys(case_dict, 'Eesti_ilukirjandus/ilukirjandus/Eesti_ilukirjandus_1990/ilu_volta.tasak.xml', 'laused.xml' )
-
-
-
-
-
-
+(combin3,combin4,sentences_with_structure,level2) = getBestPOSCombination(files)
+level1_3 = getFinalSentenceListLevel1(combin3,sentences_with_structure)
+level1_4 = getFinalSentenceListLevel1(combin4,sentences_with_structure)
+level1 = level1_3 + level1_4
+runCaseAnalys(case_dict, level1, 'lausedlevel1.xml' )
+print(level2)
+runCaseAnalys(case_dict, level2, 'lausedlevel2.xml' )
 
 
 
